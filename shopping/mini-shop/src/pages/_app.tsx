d76627c5701fe924964fda7da4bff8a642ddbf15
omidsdgi <omid69sdgi@gmail.com>
2025-04-22 12:01:36 +0330

    implement pages file

--- shopping/mini-shop/src/pages/_app.tsx
+++ shopping/mini-shop/src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import  { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {ToastContainer} from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient=new QueryClient({defaultOptions:
+        {
+          queries:
+              {
+                refetchOnWindowFocus: false,
+                refetchIntervalInBackground:false,
+              }
+        }
+  });
+  return(
+      < QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+        < ReactQueryDevtools initialIsOpen={false} />
+        <ToastContainer autoClose={5000} hideProgressBar={false}
+                        closeOnClick={false}
+                        pauseOnHover={true}
+                        draggable={false}
+                        theme="light"
+                        position="top-right"
+        />
+      </QueryClientProvider>
+  )
 }
